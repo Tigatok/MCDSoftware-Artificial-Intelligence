47368db56eafad887da36a42038865263b27e586
Tyler Marshall <tymarshall1@hotmail.com>
2014-11-27 23:50:08 -0800

    Set up directory Structure. More in google doc.

---
+++ src/mcdsoftware/ai/communication/CommunicationHandler.java
@@ -0,0 +1,7 @@
+package mcdsoftware.ai.communication;
+
+/**
+ * Created by Tyler on 27/11/2014.
+ */
+public class CommunicationHandler {
+}
---
+++ src/mcdsoftware/ai/timing/UserTimer.java
@@ -0,0 +1,8 @@
+package mcdsoftware.ai.timing;
+
+/**
+ * Created by Tyler on 27/11/2014.
+ */
+public class UserTimer extends Thread{
+
+}
